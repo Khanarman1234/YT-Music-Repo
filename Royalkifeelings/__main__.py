80b49d6683d02a07a974f586159ce85bbd8c2d6f
Khanarman1234 <135990290+Khanarman1234@users.noreply.github.com>
2024-01-30 19:11:51 +0530

    Update __main__.py

--- Royalkifeelings/__main__.py
+++ Royalkifeelings/__main__.py
@@ -1,17 +1,17 @@
 import asyncio
 import importlib
 from pytgcalls import idle
-from Royalkifeelings.callmusic.config import BOT_USERNAME
-from Royalkifeelings import bot, call_py
-from Royalkifeelings.plugins import ALL_PLUGINS
+from K_King_HYT.callmusic.config import BOT_USERNAME
+from K_King_HYT import bot, call_py
+from K_King_HYT.plugins import ALL_PLUGINS
 
 
 loop = asyncio.get_event_loop()
 
 
-async def Royalkifeelings_boot():
+async def K_King_HYT():
     for all_module in ALL_PLUGINS:
-        importlib.import_module("Royalkifeelings.plugins." + all_module)
+        importlib.import_module("K_King_HYT.plugins." + all_module)
     await bot.start()
     await call_py.start()
     await idle()
@@ -20,4 +20,4 @@ async def Royalkifeelings_boot():
 
 
 if __name__ == "__main__":
-    loop.run_until_complete(Royalkifeelings_boot())
+    loop.run_until_complete(K_King_HYT())
